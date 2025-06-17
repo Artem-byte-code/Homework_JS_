55018e7df5a5778e34c42d0623b873fbf1dfbff8
a.boyarincev <a.boyarincev@pfsz.ru>
2025-06-17 17:01:53 +0700

    Сделал почти весь 4 урок

---
+++ All_homework/Базовый_JS_homework/dz_4_Boyarincev_Base_JS_/3_практическое_/Game.js
@@ -0,0 +1,42 @@
+Questions:[
+        {question_1: "Сколько градусов кипение воды?"},
+        {question_2: "Какое самое глубокое озеро в мире?"},
+        {question_3: ""},
+        {question_4: ""},
+        {question_5: ""}
+    ]
+
+    AnswersToQuestion_1:[
+        {answer_1: "100 градусов"},
+        {answer_2: "50 градусов"},
+        {answer_3: "101 градус"},
+        {answer_4: "80 градусов"}
+    ];
+
+    AnswersToQuestion_2:[
+        {answer_1: ""},
+        {answer_1: ""},
+        {answer_1: ""},
+        {answer_1: ""}
+    ];
+
+    AnswersToQuestion_3:[
+        {answer_1: ""},
+        {answer_2: ""},
+        {answer_3: ""},
+        {answer_4: ""}
+    ];
+
+        AnswersToQuestion_4:[
+        {answer_1: ""},
+        {answer_1: ""},
+        {answer_1: ""},
+        {answer_1: ""}
+    ];
+
+        AnswersToQuestion_5:[
+        {answer_1: ""},
+        {answer_1: ""},
+        {answer_1: ""},
+        {answer_1: ""}
+    ];
